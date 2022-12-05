4079a7d55e7fe9060325f882ce40bcb1972b7ec9
taiseih <taisei.hayashizaki@gmail.com>
2022-12-05 22:55:25 +0900

    コメントアウトの削除

--- resources/views/book/edit.blade.php
+++ resources/views/book/edit.blade.php
@@ -1,41 +1,5 @@
-<!--<head>
-    <title>Laravel Sample</title>
-    //bootstrapの読み込み//
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-</head>-->
-
 @extends('book/layout')
 @section('content')
-@include('book/form', ['target' => 'update'])　{{--form.blade.phpの内容を@include（名称わからず）で挿入した時に引数に指定した第1引数target変数に第2引数update変数の内容を代入する--}}
+{{--form.blade.phpの内容を@includeで挿入した時に引数に指定した第1引数target変数に第2引数update変数の内容を代入する--}}
+@include('book/form', ['target' => 'update']) 
 @endsection
-    <!--<div class="container ops-main">
-    <div class="row">
-        <div class="col-md-6">
-            <h2>書籍登録</h2>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-md-8 col-md-offset-1">
-            <form action="/book/{{ $book->id }}" method="post">
-                <!-- updateメソッドにはPUTメソッドがルーティングされているのでPUTにする --
-                <input type="hidden" name="_method" value="PUT">
-                <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                <div class="form-group">
-                    <label for="name">書籍名</label>
-                    <input type="text" class="form-control" name="name" value="{{ $book->name }}">
-                </div>
-                <div class="form-group">
-                    <label for="price">価格</label>
-                    <input type="text" class="form-control" name="price" value="{{ $book->price }}">
-                </div>
-                <div class="form-group">
-                    <label for="author">著者</label>
-                    <input type="text" class="form-control" name="author" value="{{ $book->author }}">
-                </div>
-                <button type="submit" class="btn btn-default">登録</button>
-                <a href="/book">戻る</a>
-            </form>
-        </div>
-    </div>
-
-</div>-->
